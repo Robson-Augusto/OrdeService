ee231047c287eca1475821847aeabba8290e2a8c
Robson-Augusto <99415154+Robson-Augusto@users.noreply.github.com>
2023-07-04 15:40:41 -0300

    Video aula 7

--- src/main/java/com/project/domain/Cliente.java
+++ src/main/java/com/project/domain/Cliente.java
@@ -1,7 +1,20 @@
 package com.project.domain;
 
-public class Cliente extends Pessoa{
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa implements Serializable{
+	private static final long serialVersionUID =1L;
+	
+
+	@OneToMany(mappedBy = "cliente")
+	private List<OS> list = new ArrayList<>();
+	
 	public Cliente() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -10,6 +23,14 @@ public class Cliente extends Pessoa{
 	public Cliente(Integer id, String nome, String cpf, String telefone) {
 		super(id, nome, cpf, telefone);
 		// TODO Auto-generated constructor stub
+	}
+
+	public List<OS> getList() {
+		return list;
+	}
+
+	public void setList(List<OS> list) {
+		this.list = list;
 	}
 	
 	
